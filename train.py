8cd435ca8e0d4b3719fc2e5798f2e044f838b5cd
Bigmatrix2 <76942486+Bigmatrix2@users.noreply.github.com>
2023-12-13 17:31:03 +0100

    Update train.py

--- train.py
+++ train.py
@@ -5,10 +5,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report
 from sklearn.preprocessing import LabelEncoder
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.impute import SimpleImputer
-from sklearn.pipeline import Pipeline
 
 # Divisez votre dataset en ensembles d'entraînement et de test
 X = clean_df_train(df_train).drop('Survived', axis=1)
@@ -19,24 +15,6 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 label_encoder = LabelEncoder()
 X_train['Name'] = label_encoder.fit_transform(X_train['Name'])
 X_test['Name'] = label_encoder.fit_transform(X_test['Name'])
-
-numerical_columns = ['Age']
-
-numerical_transformer = Pipeline(steps=[
-    ('imputer', SimpleImputer(strategy='mean'))  # Remplace les valeurs manquantes par la moyenne
-])
-
-preprocessor = ColumnTransformer(
-    transformers=[
-        ('cat', categorical_transformer, categorical_columns),
-        ('num', numerical_transformer, numerical_columns)
-    ]
-)
-
-numerical_transformer = Pipeline(steps=[
-    ('imputer', SimpleImputer(strategy='most_frequent')),  # Remplace les valeurs manquantes par la valeur la plus fréquente
-    ('onehot', OneHotEncoder(handle_unknown='ignore'))
-])
 
 # Supprimer la colonne 'Sex' du DataFrame X_train
 X_train.drop(columns=['Sex'], inplace=True)
