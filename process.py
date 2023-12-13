82da59ba1eda7f3ee34f61efc220b3b71683e6c4
Bigmatrix2 <76942486+Bigmatrix2@users.noreply.github.com>
2023-12-13 15:45:11 +0100

    Update process.py

--- process.py
+++ process.py
@@ -2,13 +2,11 @@
 import pandas as pd
 
 def clean_df_train(df_train):
-    # Identifiez et traitez les valeurs manquantes
+  # Identifiez et traitez les valeurs manquantes
     df_train['Age'].fillna(df_train['Age'].median(), inplace=True)
     df_train['Embarked'].fillna(df_train['Embarked'].mode()[0], inplace=True)
-
   # la raison d'utilisation de fillna est qu'elle remplace les données manquantes par des données appropriées au lieu de les
   # supprimer entièrement avec dropna.
-
     return df_train
 
 
