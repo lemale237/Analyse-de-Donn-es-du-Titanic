eeb5cef368575b7a8fffbfa1225e5ceede05020d
Bigmatrix2 <76942486+Bigmatrix2@users.noreply.github.com>
2023-12-13 17:15:10 +0100

    Update train.py

--- train.py
+++ train.py
@@ -1,15 +1,67 @@
 # train.py
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report
+from sklearn.preprocessing import LabelEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
 
 # Divisez votre dataset en ensembles d'entraînement et de test
-x = df_train.drop('Survived', axis=1)
-y = df_train['Survived']
-x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X = clean_df_train(df_train).drop('Survived', axis=1)
+y = clean_df_train(df_train)['Survived']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Supposez que 'Name' est la colonne avec des noms
+label_encoder = LabelEncoder()
+X_train['Name'] = label_encoder.fit_transform(X_train['Name'])
+X_test['Name'] = label_encoder.fit_transform(X_test['Name'])
+
+numerical_columns = ['Age']
+
+numerical_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='mean'))  # Remplace les valeurs manquantes par la moyenne
+])
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('cat', categorical_transformer, categorical_columns),
+        ('num', numerical_transformer, numerical_columns)
+    ]
+)
+
+numerical_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='most_frequent')),  # Remplace les valeurs manquantes par la valeur la plus fréquente
+    ('onehot', OneHotEncoder(handle_unknown='ignore'))
+])
+
+# Supprimer la colonne 'Sex' du DataFrame X_train
+X_train.drop(columns=['Sex'], inplace=True)
+
+# Supprimer la colonne 'Sex' du DataFrame X_test
+X_test.drop(columns=['Sex'], inplace=True)
+
+# Supprimer la colonne 'Ticket' du DataFrame X_train
+X_train.drop(columns=['Ticket'], inplace=True)
+
+# Supprimer la colonne 'Ticket' du DataFrame X_test
+X_test.drop(columns=['Ticket'], inplace=True)
+
+# Supprimer la colonne 'Cabin' du DataFrame X_train
+X_train.drop(columns=['Cabin'], inplace=True)
+
+# Supprimer la colonne 'Cabin' du DataFrame X_test
+X_test.drop(columns=['Cabin'], inplace=True)
+
+# Supprimer la colonne 'Embarked' du DataFrame X_train
+X_train.drop(columns=['Embarked'], inplace=True)
+
+# Supprimer la colonne 'Embarked' du DataFrame X_test
+X_test.drop(columns=['Embarked'], inplace=True)
 
 # Construisez un modèle de classification
-model = RandomForestClassifier()
+model = LogisticRegression()
 model.fit(X_train, y_train)
 
 # Évaluez la performance de votre modèle
@@ -17,5 +69,5 @@ y_pred = model.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 print(f'Accuracy: {accuracy}')
 
-# Classification report
+# Rapport de classification
 print(classification_report(y_test, y_pred))
