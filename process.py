6f0fa1f07e01e15cc0a11134df0f6fde2d9e08c4
Bigmatrix2 <76942486+Bigmatrix2@users.noreply.github.com>
2023-12-13 15:40:45 +0100

    Update process.py

--- process.py
+++ process.py
@@ -3,8 +3,8 @@ import pandas as pd
 
 def clean_df_train(df_train):
     # Identifiez et traitez les valeurs manquantes
-    df_train['Age'].fillna(df['Age'].median(), inplace=True)
-    df_train['Embarked'].fillna(df['Embarked'].mode()[0], inplace=True)
+    df_train['Age'].fillna(df_train['Age'].median(), inplace=True)
+    df_train['Embarked'].fillna(df_train['Embarked'].mode()[0], inplace=True)
 
   # la raison d'utilisation de fillna est qu'elle remplace les données manquantes par des données appropriées au lieu de les
   # supprimer entièrement avec dropna.
@@ -14,7 +14,7 @@ def clean_df_train(df_train):
 
 def clean_df_train(df_train):
     # Identifiez et traitez les valeurs manquantes
-    df_test['Age'].fillna(df['Age'].median(), inplace=True)
-    df_test['Embarked'].fillna(df['Embarked'].mode()[0], inplace=True)
+    df_test['Age'].fillna(df_test['Age'].median(), inplace=True)
+    df_test['Embarked'].fillna(df_test['Embarked'].mode()[0], inplace=True)
 
     return df_test
