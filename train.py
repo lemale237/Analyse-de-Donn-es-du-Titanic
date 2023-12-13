532d064f799662a9410716153b2aac8520694a35
Bigmatrix2 <76942486+Bigmatrix2@users.noreply.github.com>
2023-12-13 15:35:18 +0100

    Update train.py

--- train.py
+++ train.py
@@ -1,2 +1,22 @@
+# train.py
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report
 
-ylh
+# Divisez votre dataset en ensembles d'entraînement et de test
+X = df_cleaned.drop('Survived', axis=1)
+y = df_cleaned['Survived']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Construisez un modèle de classification
+model = RandomForestClassifier()
+model.fit(X_train, y_train)
+
+# Évaluez la performance de votre modèle
+y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+print(f'Accuracy: {accuracy}')
+
+# Classification report
+print(classification_report(y_test, y_pred))
+
