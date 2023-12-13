d6261f10a8b3b6c37d5bf4b43af2da24f49ce735
Bigmatrix2 <76942486+Bigmatrix2@users.noreply.github.com>
2023-12-13 15:58:14 +0100

    Update train.py

--- train.py
+++ train.py
@@ -4,8 +4,8 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report
 
 # Divisez votre dataset en ensembles d'entraînement et de test
-X = df_cleaned.drop('Survived', axis=1)
-y = df_cleaned['Survived']
+X = df_train.drop('Survived', axis=1)
+y = df_train['Survived']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Construisez un modèle de classification
@@ -19,4 +19,3 @@ print(f'Accuracy: {accuracy}')
 
 # Classification report
 print(classification_report(y_test, y_pred))
-
